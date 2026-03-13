5d3ede6e6aaa1e79c15aa4391ddba79746e66ca1
siddharthg-7 <siddharthgoudgilakathi@gmail.com>
2026-03-13 07:51:53 +0530

    feat: add navigation links to Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,29 @@
 import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       <img src={assets.logo} className="w-16" alt="Logo" />
+
+      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+        <NavLink to="/" className='flex flex-col items-center gap-1'>
+          <p>Home</p>
+          <hr className='w-24 border-none h-[1.5px] bg-gray-700'/>
+        </NavLink>
+         <NavLink to="/Collection" className='flex flex-col items-center gap-1'>
+          <p>Collection</p>
+          <hr className='w-24 border-none h-[1.5px] bg-gray-700'/>
+        </NavLink>
+         <NavLink to="/About" className='flex flex-col items-center gap-1'>
+          <p>About</p>
+          <hr className='w-24 border-none h-[1.5px] bg-gray-700'/>
+        </NavLink>
+         <NavLink to="/Contact" className='flex flex-col items-center gap-1'>
+          <p>Contact</p>
+          <hr className='w-24 border-none h-[1.5px] bg-gray-700'/>
+        </NavLink>
+      </ul>
     </div>
   )
 }
