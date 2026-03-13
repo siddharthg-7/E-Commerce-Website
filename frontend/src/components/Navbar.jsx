c23a6702b59e24f9e0a5579ffb194243a5b59c98
siddharthg-7 <siddharthgoudgilakathi@gmail.com>
2026-03-13 14:23:58 +0530

    feat: enhance Navbar with dropdown menu and update styling for active links

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,27 +3,50 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between py-5 font-medium'>
+    <div className='flex items-center justify-between py-5 font-medium'>  
       <img src={assets.logo} className="w-16" alt="Logo" />
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
         <NavLink to="/" className='flex flex-col items-center gap-1'>
           <p>Home</p>
-          <hr className='w-24 border-none h-[1.5px] bg-gray-700'/>
+          <hr className='w-24 border-none h-[1.5px] bg-gray-700 hidden'/>
         </NavLink>
-         <NavLink to="/Collection" className='flex flex-col items-center gap-1'>
+
+        <NavLink to="/Collection" className='flex flex-col items-center gap-1'>
           <p>Collection</p>
-          <hr className='w-24 border-none h-[1.5px] bg-gray-700'/>
+          <hr className='w-24 border-none h-[1.5px] bg-gray-700 hidden'/>
         </NavLink>
-         <NavLink to="/About" className='flex flex-col items-center gap-1'>
+
+        <NavLink to="/About" className='flex flex-col items-center gap-1'>
           <p>About</p>
-          <hr className='w-24 border-none h-[1.5px] bg-gray-700'/>
+          <hr className='w-24 border-none h-[1.5px] bg-gray-700 hidden'/>
         </NavLink>
-         <NavLink to="/Contact" className='flex flex-col items-center gap-1'>
+
+        <NavLink to="/Contact" className='flex flex-col items-center gap-1'>
           <p>Contact</p>
-          <hr className='w-24 border-none h-[1.5px] bg-gray-700'/>
+          <hr className='w-24 border-none h-[1.5px] bg-gray-700 hidden'/>
         </NavLink>
       </ul>
+
+      <div className='flex items-center gap-5 text-gray-700'>
+        <img src={assets.search_icon} className='w-5 cursor-pointer' alt="Search" />
+
+        <div className='group relative'>
+          <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="Profile" />
+
+          {/* Dropdown */}
+          <div className='group-hover:block hidden absolute right-0 pt-4'>
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+              <p className='cursor-pointer hover:text-black'>My Profile</p>
+              <p className='cursor-pointer hover:text-black'>Orders</p>
+              <p className='cursor-pointer hover:text-black'>Logout</p>
+            </div>
+          </div>
+
+        </div>
+        
+
+      </div>
     </div>
   )
 }
