701c63a298886e51469624be9b2aad7769351e0e
siddharthg-7 <siddharthgoudgilakathi@gmail.com>
2026-03-17 22:34:16 +0530

    feat: add Hero component for showcasing latest arrivals

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets } from '../assets/assets.js'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border border-gray-400'>
+        {/* Hero left side */}
+        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+        <div className='text-[#414141]'>
+
+          <div className='flex items-center gap-2'>
+            <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+            <p className='font-medium text-sm md:text-base'>Our BestSellers</p>
+          </div>
+
+          <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>
+            Latest Arrivals
+          </h1>
+
+          <div className='flex items-center gap-2'>
+           <p className='font-semibold text-sm md:text-base'>Shop Now</p>
+           <p></p>
+          </div>
+
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Hero
