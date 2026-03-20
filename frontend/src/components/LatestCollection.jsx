88349ae46fba16472e99888b68c61489c8d92f90
siddharthg-7 <siddharthgoudgilakathi@gmail.com>
2026-03-20 23:56:48 +0530

    Added Latest Collection File

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const LatestCollection = () => {
+    const {products} = useContext(ShopContext);
+  return (
+    <div>LatestCollection</div>
+  )
+}
+
+export default LatestCollection
