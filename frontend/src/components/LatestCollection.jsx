8d8b9dd8cbf5c252ef3076cc0517810064ee31d9
siddharthg-7 <siddharthgoudgilakathi@gmail.com>
2026-03-21 12:46:06 +0530

    Checkpoint from VS Code for cloud agent session

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,11 +1,17 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Tittle from './Tittle';
 
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
+  
   return (
-    <div>LatestCollection</div>
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Tittle text1={'LATEST'} text2={'COLLECTION'} />
+      </div>
+    </div>
   )
 }
 
