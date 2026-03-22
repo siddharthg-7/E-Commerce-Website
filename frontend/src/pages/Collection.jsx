4d4cadb34f05d772c9e87e297b51fef2bc68fc75
siddharthg-7 <siddharthgoudgilakathi@gmail.com>
2026-03-23 00:35:21 +0530

    Filters in Collection.jsx

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -2,9 +2,41 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 
 const Collection = () => {
-  const {products} = useContext(ShopContext);
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = React.useState(false);
+
   return (
-    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'></div>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+      
+      {/* Filter Options */}
+      <div className='min-w-60'>
+        
+        <p 
+          className='my-2 text-xl flex items-center cursor-pointer gap-2'
+          onClick={() => setShowFilter(prev => !prev)}
+        >
+          FILTERS
+        </p>
+
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+          {/* Your filter content */}
+          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+           <p className='flex-gap-2'>
+            <input  className='w-3 h-3' type="checkbox" value={'Men'}/>Men
+           </p>
+           <p className='flex-gap-2'>
+            <input  className='w-3 h-3' type="checkbox" value={'Women'}/>Women
+           </p>
+           <p className='flex-gap-2'>
+            <input  className='w-3 h-3' type="checkbox" value={'Kids'}/>Kids
+           </p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
