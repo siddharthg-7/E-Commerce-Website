559bd190cc3e32f7c50dfb0117a4857bb4c023d3
siddharthg-7 <siddharthgoudgilakathi@gmail.com>
2026-04-02 22:05:21 +0530

    feat: implement user login and registration with JWT and bcrypt

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,8 +1,13 @@
-import { json } from "express"
 import validator from "validator";
 import userModel from "../models/userModel.js";
-const loginUser = async (req, res) => {
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
+const createToken = (user) => {
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+}
+const loginUser = async (req, res) => {
+    res.status(501).json({ success: false, message: 'Login not implemented yet' });
 }
 
 const registerUser = async (req, res) => {
@@ -19,13 +24,26 @@ const registerUser = async (req, res) => {
         if (password.length < 8) {
             return res.status(400).json({  success: false, message: 'Password must be at least 8 characters long' });
         }
+
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        });
+        const user = await newUser.save();
+        const token = createToken(user._id);
+        res.json({ success: true, token });
     }catch(error){
         console.error('Error registering user:', error);
         res.status(500).json({  success: false, message: 'Server error' });
     }
 
 }
-//route for admin login 
+
+
 const adminLogin = async (req, res) => {
 
 }
