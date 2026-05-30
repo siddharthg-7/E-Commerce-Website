67e7fb790d660c1c67104e928713cb06d2c7b046
siddharthg-7 <siddharthgoudgilakathi@gmail.com>
2026-05-30 11:39:07 +0530

    feat: update file upload labels for improved accessibility and structure

--- admin/admin/src/pages/add.jsx
+++ admin/admin/src/pages/add.jsx
@@ -10,11 +10,25 @@ const add = () => {
           Upload image 
         </p>
         <div> 
-          <label htmlFor="fileInput" className='cursor-pointer bg-gray-200 px-3 py-1 rounded-md text-sm'>
-            <img src={assets.upload_area} alt="Upload Icon" className='w-5 h-5 inline-block mr-2' />
+          <label htmlFor="image1" >
+            <img src={assets.upload_area} alt="Upload Icon"  />
             Choose File
-            <input id="fileInput" type="file" className='hidden' />
-
+            <input id="image1" type="file" className='hidden' />
+          </label>
+          <label htmlFor="image2" >
+            <img src={assets.upload_area} alt="Upload Icon"  />
+            Choose File
+            <input id="image2" type="file" className='hidden' />
+          </label>
+          <label htmlFor="image3" >
+            <img src={assets.upload_area} alt="Upload Icon"  />
+            Choose File
+            <input id="image3" type="file" className='hidden' />
+          </label>
+          <label htmlFor="image4" >
+            <img src={assets.upload_area} alt="Upload Icon"  />
+            Choose File
+            <input id="image4" type="file" className='hidden' />
           </label>
 
         </div>
